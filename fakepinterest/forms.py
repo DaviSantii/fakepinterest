3e6e80ee2901ed1cd0b61ff1cc7a80e744f6785d
DaviSantii <davi_s@id.uff.br>
2024-07-16 00:39:57 -0300

    primeiro commit

---
+++ fakepinterest/forms.py
@@ -0,0 +1,40 @@
+from collections.abc import Sequence
+from typing import Any, Mapping
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, FileField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import Usuario
+
+
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao = SubmitField("Fazer Login")
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if not usuario:
+            raise ValidationError("Email inserido errado ou inexistente")
+
+
+class FormCriarConta(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    username = StringField("Nome de Usuário", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField("Confirmação de Senha", validators=[DataRequired(), EqualTo("senha")])
+    botao = SubmitField("Criar Conta")
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            raise ValidationError("E-mail já cadastrado, faça login para continuar")
+
+    def validate_username(self, username):
+        usuario = Usuario.query.filter_by(username=username.data).first()
+        if usuario:
+            raise ValidationError("Nome de Usuário já cadastrado")
+
+
+class FormFoto(FlaskForm):
+    foto = FileField("Foto", validators=[DataRequired()])
+    botao = SubmitField("Enviar")
